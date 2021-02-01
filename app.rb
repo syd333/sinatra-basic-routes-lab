7eef675044ecdb5a28c0b4cd72f0fac01002b13d
syd333 <sydmiranda@icloud.com>
2021-01-31 19:36:18 -0800

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Sydney"
+      end 
+
+      get '/hometown' do
+        "My hometown is Buffalo"
+      end 
+
+      get '/favorite-song' do
+        "My favorite song is something"
+      end 
 end
+
